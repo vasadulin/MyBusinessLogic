940c4b48d52e164cae96751bb1361869a625ca58
Asadulin Viktor <vasadulin@gmail.com>
2022-12-06 00:08:42 +0200

    Add public modifiers

--- Sources/MyBusinessLogic/MyBusinessLogic.swift
+++ Sources/MyBusinessLogic/MyBusinessLogic.swift
@@ -1,8 +1,9 @@
 
 public class DummyLogic {
   
-   func calculate(first: String, second: String) -> String {
-      return "Remote logic: " + first + second
-   }
+  public func calculate(first: String, second: String) -> String {
+    return "Remote logic: " + first + second
+  }
   
+  public init() {}
 }
